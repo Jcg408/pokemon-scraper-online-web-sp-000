5f9458d5cc20e2fcb7bfc0dcb60a829475eb54b1
Jessica Gladstone <jcg321@comcast.net>
2019-01-25 18:47:32 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,8 @@ class Pokemon
   end
   
   def self.find (id, db)
-     pokemon_id = db.execute("SELECT * FROM pokemon WHERE id =?", id)
+     pokemon = db.execute("SELECT * FROM pokemon WHERE id =?", id)
+     pokemon
      end
     # binding.pry
  
