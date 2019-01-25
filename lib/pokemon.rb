47995b00c26e5bb073c48ddcb2c6b19cee91a8c9
Jessica Gladstone <jcg321@comcast.net>
2019-01-25 19:10:59 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,11 @@ class Pokemon
   end
   
   def self.find (id, db)
-     pokemon = db.execute("SELECT * FROM pokemon WHERE id =?", id).flatten
+      db.execute("SELECT * FROM pokemon WHERE id =?", id).flatten.each do |pokemon|
+        attribute[0] = id
+        attribute[1] = name
+        attribute[3] = type
+      end
     # binding.pry
  
   end
